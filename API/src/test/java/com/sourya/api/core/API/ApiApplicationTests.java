a1c7256d71e34fc46c8742a53e5434b868fefa88
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-04 09:19:38 +0530

    adding utils

---
+++ API/src/test/java/com/sourya/api/core/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourya.api.core.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ util/src/main/java/com/sourya/util/http/util/UtilApplication.java
@@ -0,0 +1,13 @@
+package com.sourya.util.http.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilApplication.class, args);
+	}
+
+}
