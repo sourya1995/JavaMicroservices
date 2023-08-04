b511c02936b9533bde0bb34f7e5834c43fb5ead4
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-08-04 08:57:10 +0530

    initial microservices

---
+++ ReviewService/src/test/java/com/sourya/microservices/core/review/ReviewService/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourya.microservices.core.review.ReviewService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
